8dc164f46733b19bad11b02f5a99ad5d53212bd5
chuanqin <callforsky@gmail.com>
2017-08-05 11:22:18 -0400

    Update 2nd version
    stuck on recursion function

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -18,13 +18,14 @@ lvl3_Ans = ['2003', 'Yang Liwei', '2008', 'Zhai Zhigang']
 
 # Open game, prompted to select the level, then run the relevant level based on the choice
 def PlayGame():
-    ''' Ask the player to select a difficulty level, then run the corresponding game  '''
+    ''' Ask the player to select a difficulty level, and how many guesses they can make before they lose, then run the corresponding game  '''
     #defensive programming, prompt the user to enter the valid value
     while True:
         try:
             choice = int(raw_input("Welcome! I am going to test your knowledge of the history of Space Exploration! This game has 3 levels: 1-easy; 2-medium; 3-hard. Please enter the number to choose your challenge --> "))
+            guess_num = int(raw_input("Please enter the number of guesses you can make before you lose --> "))
         except ValueError:
-            print "You must enter 1, 2, or 3 only!"
+            print "You must enter a valid integer!"
             continue
 
         if choice not in range(1,4):
@@ -35,14 +36,14 @@ def PlayGame():
             break
     # run the game based on player's difficulty choice
     if choice == 1:
-        return QuestionAndAnswer(lvl1, lvl1_Ans)
+        return QuestionAndAnswer(lvl1, lvl1_Ans, guess_num)
     elif choice == 2:
-        return QuestionAndAnswer(lvl2, lvl2_Ans)
+        return QuestionAndAnswer(lvl2, lvl2_Ans, guess_num)
     else:
-        return QuestionAndAnswer(lvl3, lvl3_Ans)
+        return QuestionAndAnswer(lvl3, lvl3_Ans, guess_num)
 
-def QuestionAndAnswer(question, answer):
-    ''' Display the question at first. Ask the player to fill in the blank one by one. The game will not move to next blank until the user enter the correct answer '''
+def QuestionAndAnswer(question, answer, guess_num):
+    ''' Display the question at first. Ask the player to fill in the blank one by one. The player will have X guesses before they lose. X = number of allowed guesses, set by the player earlier '''
     print question[1]
     # will use the item_map below to translate item code to English language. we use it to tell player which blank they are on
     item_map = {1: 'first', 2:'second', 3:'third', 4:'fourth'}
@@ -51,16 +52,28 @@ def QuestionAndAnswer(question, answer):
         if "____" in word:
             # loop on the same blank until the player enter the correct answer, I give the player 3 guesses, if not correct after 3 guesses, prompt the answer
             guesses = 0
-            while guesses < 3:
+            while guesses < guess_num:
                 response = raw_input("Please fill in the " + item_map[item] + " blank: ")
-                guesses += 1
                 if response != answer[item-1]:
+                    guesses += 1
                     print "Sorry, wrong answer. Please try again."
                 else:
                     print "You're correct! The " + item_map[item] + " blank should be " + answer[item-1]
-                    # move to next blank, update the item order
+                    # move to next blank, update the item order and reset guesses
                     item += 1
+                    guesses = 0
                     break
+            print guesses
+            print guess_num
+            if guesses == guess_num:
+                print "Sorry, you guessed " + str(guess_num) + " times wrong. You lost this game! Please restart this game."
+                QuestionAndAnswer(question, answer, guess_num)
+                # codes below: explore another way of playing this game
+                # print "move to next blank"
+                # item += 1
+                # continue
+            else:
+                print "Here!"
 
     return "Congratulation! You passed Level " + question[0] + " of this game!"
 
